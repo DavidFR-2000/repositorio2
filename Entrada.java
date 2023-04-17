d7f4defe8ea5e5376ce7dba9da238fa8a9d56c7c
chvstx <lolmanster241@gmail.com>
2023-04-17 13:17:48 +0200

    Incidencia 1

--- Entrada.java
+++ Entrada.java
@@ -1,9 +1,5 @@
 package org.example;
 
-public class Main {
-
-    public static void main(String[] args) {
-	System.out.println("Procesando");
-        Salida.escribir();
-    }
+public class Entrada{
+	
 }
