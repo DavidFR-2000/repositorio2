d640884bb559312078c74030334628291d6400ae
chvstx <lolmanster241@gmail.com>
2023-04-17 13:29:25 +0200

    Incidencia 2

--- main.java
+++ main.java
@@ -3,7 +3,8 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Procesando");
+	System.out.println("Computando");
         Salida.escribir();
+    System.out.println("Fin de programa");
     }
 }
